9d01227e9334e882a07d02e69143a37e8f511a2c
kaufman79 <kaufman79@gmail.com>
2024-06-17 14:39:59 -0400

    adds placeholders, etc

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
+import pandas
 
-app = Flask("Website")
+app = Flask(__name__)
 
 
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+    return render_template("home.html")
 
 
-app.run(debug=True)
+@app.route("/api/v1/<station>/<date>")  # < > brackets are used in Flask for user-entered values
+def about(station, date):
+    df = pandas.read_csv("")
+    temperature = df.station(date)
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
